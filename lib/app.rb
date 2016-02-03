0c762b23e04d4c567dbc331bf92f8dda5c353fe0
ViolaCrellin <viola.crellin@gmail.com>
2016-02-03 15:37:59 +0000

    Completed user story 2

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,9 @@ require 'rack'
 class Battle < Sinatra::Base
   use Rack::Session::Pool, :expire_after => 2592000
   set :sesion_secret, "super secret"
+
+  STARTING_HP = 100
+
   get '/' do
     'Hello Battle!'
   end
@@ -24,8 +27,12 @@ class Battle < Sinatra::Base
   get '/play' do
     @player_one_name = session[:player_one_name]
     @player_two_name = session[:player_two_name]
+    @player_one_hp = "HP: #{STARTING_HP}"
+    @player_two_hp = "HP: #{STARTING_HP}"
     erb :play
   end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/battle_player_entry_spec.rb
+++ spec/features/battle_player_entry_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
-feature 'Player Sign-up' do
+feature 'Player Set up' do
 
-  scenario 'When the player first joins the game' do
+  scenario 'Signing up' do
     visit '/sign_up'
     fill_in "player_one_name", with: 'Jez Corbyn'
     fill_in "player_two_name", with: 'Donald Trump'
     click_button "Submit"
     expect(page).to have_content('Jez Corbyn vs. Donald Trump')
-
   end
+
+  scenario 'Viewing player two\'s hit points as player one' do
+    visit '/play'
+    expect(page).to have_content('HP: 100')
+  end
+
 end
