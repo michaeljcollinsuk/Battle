93b5e456c8cebb4e279a95bacbe0368030ef9b39
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-02-03 12:37:00 +0000

    Session implented in app file.

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,10 @@
 require 'sinatra/base'
+require 'rack'
+# enable :sessions
 
 class Battle < Sinatra::Base
+  use Rack::Session::Pool, :expire_after => 2592000
+  set :sesion_secret, "super secret"
   get '/' do
     'Hello Battle!'
   end
@@ -12,8 +16,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb :play
   end
   # start the server if ruby file executed directly
