fe1899bc5b6730e430287c9d341b2db64394ca7b
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-04 11:30:49 +0000

    Added ok button, heath reduces after attack

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -8,4 +8,11 @@ feature 'Game play' do
     click_button "Attack@!£!$!%"
     expect(page).to have_content('Jez Corbyn attacked Donald Trump')
   end
+
+  scenario 'hp reduces' do
+    sign_in_and_play
+    click_button "Attack@!£!$!%"
+    click_button "OK"
+    expect(page).to have_content('HP: 90')
+  end
 end
